c4d84fdb1a6e6983ea4dd14a6bc5bbc77faba887
Panth <panth977@Panths-MacBook-Pro.local>
2024-10-27 21:39:50 +0530

    added module descriptions with examples

--- mod.ts
+++ mod.ts
@@ -1,4 +1,13 @@
 /**
+ * This are tools from Panth977, who has certain design principals in coding! this makes his life in coding very very easy! 🎉
+ * @module
+ * 
+ * @example
+ * import {hiMomOf} from "@panth977/tools"
+ * hiMomOf('Panth') // Hi Panth's mom
+ */
+
+/**
  * This function allows you to "Hi" mom of the target person!
  * @param name target person
  * @example 
