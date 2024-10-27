65bf3d14b8b37d50245f58b563ddbfd6b88f51a4
Panth <panth977@Panths-MacBook-Pro.local>
2024-10-27 21:29:05 +0530

    added js doc to exported functions

--- mod.ts
+++ mod.ts
@@ -1,3 +1,9 @@
+/**
+ * This function allows you to "Hi" mom of the target person!
+ * @param name target person
+ * @example 
+ * hiMomOf('panth') // Hi ${name}'s mom
+ */
 export function hiMomOf(name: string) {
     console.log(`Hi ${name}'s mom`);
 }
