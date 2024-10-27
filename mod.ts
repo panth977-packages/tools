c7e98cd413665096db83a3d3edf32adf9776bc1b
Panth <panth977@Panths-MacBook-Pro.local>
2024-10-27 22:06:16 +0530

    v0.1.11

--- mod.ts
+++ mod.ts
@@ -1,21 +1,13 @@
 /**
- * This are tools from Panth977, who has certain design principals in coding! this makes his life in coding very very easy! 🎉
+ * These are tools from Panth977, who has certain design principles in coding! This makes his life in coding very very easy! 🎉
  * @module
  *
  * @example
  * ```ts
- * import TOOLS from "@panth977/tools";
+ * import { TOOLS } from "@panth977/tools";
  *
  * TOOLS.hiMomOf('Panth'); // Hi Panth's mom
  * ```
  */
 
-/**
- * The `TOOLS` namespace provides a convenient way to access various utility functions and modules.
- * It re-exports everything from `exports.ts`, allowing you to use `TOOLS` as a single point of access
- * for all tools within this module.
- *
- * @namespace TOOLS
- * @exports TOOLS
- */
 export * as TOOLS from "./exports.ts";
