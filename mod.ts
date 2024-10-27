cf05e92106a518cd6c21b1232a162e945926b254
Panth <panth977@Panths-MacBook-Pro.local>
2024-10-27 21:36:29 +0530

    removing module descriptions

--- mod.ts
+++ mod.ts
@@ -1,9 +1,4 @@
 /**
- * This are tools from Panth977, who has certain design principals in coding! this makes his life in coding very very easy! 🎉
- * @module
- */
-
-/**
  * This function allows you to "Hi" mom of the target person!
  * @param name target person
  * @example 
